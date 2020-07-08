3e88be57abc44fa0a0cac316b5a2c60fa970e574
Henrique Gonçalves Parreiras <henrique.parreiras@bhz.vettatech.com>
2020-07-08 14:26:24 -0300

    Blogs

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,19 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    console.log('About to fetch posts!');
+    await dispatch(fetchPosts());
+    const uniqueUsers = [...new Set(getState().posts.map(post => post.userId))];
+    uniqueUsers.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
 };
